4ced561fcea02f5cbc379aa3a7603ff7e1381a74
DinoCzar <ryanb_booking@yahoo.com>
2023-04-25 00:06:09 -0700

    Add field validation

--- script.js
+++ script.js
@@ -22,79 +22,84 @@ newBook.addEventListener('click', (e) => {
 });
 
 submitButton.addEventListener('click', (e) => {
-	const newBook = new Book(title.value, author.value, pages.value);
-
-	modal.style.display = 'none';
-	overlay.style.display = 'none';
-
-	const newCard = document.createElement('div');
-	newCard.classList.add('new-card');
-	newCard.dataset.number = myLibrary.indexOf(newBook);
-	bookDisplay.appendChild(newCard);
-
-	const titleDiv = document.createElement('div');
-	titleDiv.textContent = ' " ' + newBook.title + ' " ';
-	titleDiv.classList.add('title-div');
-	newCard.appendChild(titleDiv);
-
-	const authorDiv = document.createElement('div');
-	authorDiv.textContent = 'by ' + newBook.author;
-	authorDiv.classList.add('author-div');
-	newCard.appendChild(authorDiv);
-
-	const pagesDiv = document.createElement('div');
-	pagesDiv.textContent = newBook.pages + ' pages';
-	pagesDiv.classList.add('pages-div');
-	newCard.appendChild(pagesDiv);
-
-	const buttonsDiv = document.createElement('div');
-	buttonsDiv.classList.add('buttons-div');
-	newCard.appendChild(buttonsDiv);
-
-	const readButton = document.createElement('button');
-	readButton.classList.add('read-button');
-	readButton.textContent = 'Read';
-	buttonsDiv.appendChild(readButton);
-
-	const notReadButton = document.createElement('button');
-	notReadButton.classList.add('not-read-button');
-	notReadButton.textContent = 'Not Read';
-	buttonsDiv.appendChild(notReadButton);
-
-	const removeButton = document.createElement('button');
-	removeButton.classList.add('remove-button');
-	removeButton.textContent = 'Remove Book';
-	buttonsDiv.appendChild(removeButton);
-
-	removeButton.addEventListener('click', (e) => {
-		myLibrary.splice(newCard.dataset.number, 1);
-		newCard.remove();
-	});
-
-	if (read.checked == true) {
-		newBook.read = 'Read';
-		notReadButton.style.display = 'none';
+	if (title.value === '' || author.value === '' || pages.value === '') {
+		e.preventDefault();
+		alert('Please fill out all the fields.');
 	} else {
-		newBook.read = 'Not Read';
-		readButton.style.display = 'none';
+		const newBook = new Book(title.value, author.value, pages.value);
+
+		modal.style.display = 'none';
+		overlay.style.display = 'none';
+
+		const newCard = document.createElement('div');
+		newCard.classList.add('new-card');
+		newCard.dataset.number = myLibrary.indexOf(newBook);
+		bookDisplay.appendChild(newCard);
+
+		const titleDiv = document.createElement('div');
+		titleDiv.textContent = ' " ' + newBook.title + ' " ';
+		titleDiv.classList.add('title-div');
+		newCard.appendChild(titleDiv);
+
+		const authorDiv = document.createElement('div');
+		authorDiv.textContent = 'by ' + newBook.author;
+		authorDiv.classList.add('author-div');
+		newCard.appendChild(authorDiv);
+
+		const pagesDiv = document.createElement('div');
+		pagesDiv.textContent = newBook.pages + ' pages';
+		pagesDiv.classList.add('pages-div');
+		newCard.appendChild(pagesDiv);
+
+		const buttonsDiv = document.createElement('div');
+		buttonsDiv.classList.add('buttons-div');
+		newCard.appendChild(buttonsDiv);
+
+		const readButton = document.createElement('button');
+		readButton.classList.add('read-button');
+		readButton.textContent = 'Read';
+		buttonsDiv.appendChild(readButton);
+
+		const notReadButton = document.createElement('button');
+		notReadButton.classList.add('not-read-button');
+		notReadButton.textContent = 'Not Read';
+		buttonsDiv.appendChild(notReadButton);
+
+		const removeButton = document.createElement('button');
+		removeButton.classList.add('remove-button');
+		removeButton.textContent = 'Remove Book';
+		buttonsDiv.appendChild(removeButton);
+
+		removeButton.addEventListener('click', (e) => {
+			myLibrary.splice(newCard.dataset.number, 1);
+			newCard.remove();
+		});
+
+		if (read.checked == true) {
+			newBook.read = 'Read';
+			notReadButton.style.display = 'none';
+		} else {
+			newBook.read = 'Not Read';
+			readButton.style.display = 'none';
+		}
+
+		readButton.addEventListener('click', (e) => {
+			newBook.read = 'Not Read';
+			readButton.style.display = 'none';
+			notReadButton.style.display = 'block';
+		});
+
+		notReadButton.addEventListener('click', (e) => {
+			newBook.read = 'Read';
+			notReadButton.style.display = 'none';
+			readButton.style.display = 'block';
+		});
+
+		myLibrary.push(newBook);
+		title.value = '';
+		author.value = '';
+		pages.value = '';
 	}
-
-	readButton.addEventListener('click', (e) => {
-		newBook.read = 'Not Read';
-		readButton.style.display = 'none';
-		notReadButton.style.display = 'block';
-	});
-
-	notReadButton.addEventListener('click', (e) => {
-		newBook.read = 'Read';
-		notReadButton.style.display = 'none';
-		readButton.style.display = 'block';
-	});
-
-	myLibrary.push(newBook);
-	title.value = '';
-	author.value = '';
-	pages.value = '';
 });
 
 for (let i = 0; i < myLibrary.length; i++) {
